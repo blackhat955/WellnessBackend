e735c6354b579d6ac2ef37232290c14faaf98f33
Durgesh Tiwari <61361185+blackhat955@users.noreply.github.com>
2024-03-17 23:23:41 -0400

    update models and auth routher and for better working

--- models/User.js
+++ models/User.js
@@ -1,42 +1,14 @@
-// const mongoose = require('mongoose')
-// const {Schema} = mongoose
-
-// const userSchema = new Schema({
-//     name: String,
-//     email: {
-//         type: String,
-//         unique: true 
-//     },
-//     password: String,
-
-// })
-
-// const UserModel = mongoose.model('User', userSchema);
-
-// module.exports = UserModel;
-
-// models/User.js
-
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+  firstname: String,
+  lastname: String,
+  code: String,
+  resetToken: String,
+  tokenExpiry: Date,
+  type:String
 });
 
-module.exports = User = mongoose.model('user', UserSchema);
+module.exports = mongoose.model('User', userSchema);
