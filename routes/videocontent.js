7004226dd7b2968b2991b8fa0da5c8aa05e6f0a4
Durgesh Tiwari <61361185+blackhat955@users.noreply.github.com>
2024-04-16 00:02:42 -0400

    Update videocontent.js

--- routes/videocontent.js
+++ routes/videocontent.js
@@ -11,9 +11,6 @@ const fs = require('fs');
 
 // Configure AWS S3 and CloudFront
 const s3 = new AWS.S3({
-  accessKeyId:process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY ,
-  region: "us-east-1",
 });
 
 const urlfix = "dd12fyimlw2sl.cloudfront.net"; 
